f3973f87f43b890fca2c8d7919a743f87c42b144
dhara bambhaniya <bambhaniyadhara5@gmail.com>
2026-07-03 15:14:35 +0530

    task

--- 19-june/Array2.js
+++ 19-june/Array2.js
@@ -1,9 +1,6 @@
 //  1. sort() Method:
 // Question: You have an array of numbers:
 
-
-
-
 // var numbers = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5];
 
 // var sortedNumbers = [...numbers];   // Original array ki copy
